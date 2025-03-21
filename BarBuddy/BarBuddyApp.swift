1643df7b316f9587759a7a234a2f1ecc09f9eb2e
Travis Rodriguez <travisgolembiewski@gmail.com>
2025-03-21 19:59:02 -0400

    Initial commit

---
+++ BarBuddy/BarBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarBuddyApp.swift
+//  BarBuddy
+//
+//  Created by Travis Rodriguez on 3/21/25.
+//
+
+import SwiftUI
+
+@main
+struct BarBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
